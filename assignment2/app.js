a0e5dfc0f1a93b77ccfb9193eaadb7d904d1a91a
rainer <rainer.schmidt@gmail.com>
2017-09-18 16:43:19 +0200

    added assignment 2 and 3

---
+++ assignment2/app.js
@@ -0,0 +1,73 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingCheckOff', [])
+.controller('ShoppingListControllerB', ShoppingListControllerB)
+.controller('ShoppingListControllerBB', ShoppingListControllerBB)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ShoppingListControllerB.$inject = ['ShoppingListCheckOffService'];
+function ShoppingListControllerB(ShoppingListCheckOffService) {
+  var buyList = this;
+
+  buyList.items = ShoppingListCheckOffService.getItems();
+
+  buyList.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+ShoppingListControllerBB.$inject = ['ShoppingListCheckOffService'];
+function ShoppingListControllerBB(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items = ShoppingListCheckOffService.getRemovedItems();
+}
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var pn = ["apples", "bananas", "oranges"];
+  var pq = [1, 3, 4];
+  var items = [];
+  var bItems = [];
+
+  service.addItem = function (index, itemName, quantity) {
+    var item = {
+      index: index,
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
+  };
+
+  service.removeItem = function (itemIdex) {
+    bItems.push(items[itemIdex])
+    items.splice(itemIdex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+
+  service.getRemovedItems = function () {
+    return bItems;
+  };
+
+  var initItems = function (pn, pq) {
+    var items = [];
+    for (var i = 0; i < pn.length; i++) {
+      items.push({
+          name: pn[i],
+          quantity: pq[i]
+      });
+    }
+    return items;
+  };
+  items = initItems(pn, pq);
+
+}
+
+
+})();
---
+++ assignment3/app.js
@@ -0,0 +1,123 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.factory('MenuItemsListFactory', MenuItemsListFactory)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('itemList', ItemListDirective);;
+
+function ItemListDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      search: '<',
+      removeItem: '&',
+      itemsInList: '&'
+    },
+  };
+  return ddo;
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService', 'MenuItemsListFactory'];
+function NarrowItDownController(MenuSearchService, MenuItemsListFactory) {
+
+  var search = this;
+  var menuItemsList = MenuItemsListFactory();
+  search.searchTerm = "";
+
+  search.myMenuItems = function(searchTerm) {
+    console.log("myMenuItems: t="+searchTerm);
+    var foundPromise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    foundPromise.then(function(foundResponse) {
+
+      //var myObject = httpResponse.data.menu_items;
+      console.log(foundResponse);
+      menuItemsList.setItems(foundResponse);
+      search.foundItems = menuItemsList.getItems();
+    }, function(error) {    //called in the error case
+      console.log("Something went terribly wrong.");
+    });
+  };
+  search.removeItem = function (itemIndex) {
+    console.log("About to remove item: "+itemIndex);
+    menuItemsList.removeItem(itemIndex);
+  };
+  search.itemsInList = function () {
+    console.log("Items in List?: "+menuItemsList.getItems());
+    //objects do not have a .length but you can check of item[0]
+    if(menuItemsList.getItems() == undefined || menuItemsList.getItems()[0] != undefined) {
+      return true;
+    }
+    return false;
+  };
+}
+
+MenuSearchService.$inject = ['$q', '$http', 'ApiBasePath'];
+function MenuSearchService($q, $http, ApiBasePath) {
+  var service = this;
+  service.getMatchedMenuItems = function (searchTerm) {
+    var deferred = $q.defer();
+    var foundItemsResponse = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (foundItemsResponse) {
+      //console.log(foundItemsResponse);
+
+      var allFoundItems = foundItemsResponse.data.menu_items;
+      var matchedItems = [];
+      if(searchTerm === "*") { deferred.resolve(allFoundItems);
+        } else {
+        //console.log("afi: "+allFoundItems);
+        for(var index in allFoundItems) {
+          var item = allFoundItems[index];
+          if(item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) > 0) matchedItems.push(item);
+          //console.log("prompName: "+item.name);
+        }
+      }
+      deferred.resolve(matchedItems);
+    }).catch(function (foundItemsResponse) {
+      console.log("Something went terribly wrong. "+foundItemsResponse);
+      deferred.reject(foundItemsResponse)
+    });
+
+    //console.log("found items: "+foundItemsResponse);
+    //myObject is undefined here
+    //console.log("myObject: "+myObject);
+    return deferred.promise;
+  };
+}
+
+function MenuItemsListService() {
+  var itemsListService = this;
+  var items;
+
+  // List of shopping items
+  //var items = [];
+
+  itemsListService.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
+  };
+
+  itemsListService.getItems = function () {
+    return items;
+  };
+
+  itemsListService.setItems = function (foundItems) {
+    items = foundItems;
+  };
+}
+
+
+function MenuItemsListFactory() {
+  var factory = function () {
+    return new MenuItemsListService();
+  };
+
+  return factory;
+}
+
+})();
